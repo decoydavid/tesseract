71af3e184906c05fb86981860e14ce12f5b3c4b3
Simon Cross <hodgestar@gmail.com>
2017-03-30 23:32:28 +0200

    Add skeleton art renderer.

---
+++ ract/art/__init__.py
@@ -0,0 +1,7 @@
+""" Package for tesseract art renders. """
+
+from .one_voxel_scan import OneVoxelScan
+
+ART = [
+    OneVoxelScan,
+]
---
+++ ract/art/one_voxel_scan.py
@@ -0,0 +1,15 @@
+""" Move one lit voxel around the full cube. """
+
+from ract.interfaces import ArtworkRenderer
+
+
+class OneVoxelScan(ArtworkRenderer):
+    """ Move one lit voxel around the full cube. """
+
+    NAME = 'one-voxel-scan'
+
+    def setup(self, fps):
+        pass
+
+    def next_frame(self):
+        pass
