837128741f391ef4d3b524d77cdf07017f17e138
Simon Cross <hodgestar@gmail.com>
2017-04-06 00:40:04 +0200

    Add 3-D game of life to registry.

--- ract/art/__init__.py
+++ ract/art/__init__.py
@@ -2,8 +2,10 @@
 
 from .one_voxel_scan import OneVoxelScan
 from .game_of_life_2d import GameOfLife2D
+from .game_of_life_3d import GameOfLife3D
 
 ART = [
     OneVoxelScan,
     GameOfLife2D,
+    GameOfLife3D,
 ]
