f071bf103a5e14fecd2ab60149c24b61d2e9b7f4
Simon Cross <hodgestar@gmail.com>
2017-04-08 22:56:28 +0200

    Add all LEDs on.

--- ract/art/__init__.py
+++ ract/art/__init__.py
@@ -1,10 +1,12 @@
 """ Package for tesseract art renders. """
 
+from .all_on import AllOn
 from .one_voxel_scan import OneVoxelScan
 from .game_of_life_2d import GameOfLife2D
 from .game_of_life_3d import GameOfLife3D
 
 ART = [
+    AllOn,
     OneVoxelScan,
     GameOfLife2D,
     GameOfLife3D,
