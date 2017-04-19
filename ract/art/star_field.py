4b05cedd04094b1ca2fecf01eb8651dc74bdcd01
David Sharpe <decoydavid@gmail.com>
2017-04-19 23:08:00 +0200

    Star field art

--- ract/art/star_field.py
+++ ract/art/star_field.py
@@ -2,6 +2,8 @@
 
 import itertools
 from random import random, randint
+from math import ceil, floor
+import numpy as np
 
 from ract.interfaces import ArtworkRenderer
 from ract.utils import (
@@ -20,8 +22,8 @@ class StarField(ArtworkRenderer):
     """ Does what it says on the box. """
 
     NAME = 'star-field'
-    SECONDS_PER_STEP = 0.1
-    TOTAL_STARS = 50
+    SECONDS_PER_STEP = 0.05
+    TOTAL_STARS = 70
 
     def __init__(self):
         self._stepper = TimeStepper(self.SECONDS_PER_STEP)
@@ -31,13 +33,29 @@ class StarField(ArtworkRenderer):
     def setup(self, fps):
         self._stepper = TimeStepper(self.SECONDS_PER_STEP)
         for i in range(self.TOTAL_STARS):
-            self._stars.append(Star(randint(0, 7), randint(0, 7), randint(0, 7), random() * 0.3))
+            self._stars.append(Star(random() * 7, random() * 7, random() * 7, random() * 0.1 + 0.1))
         self._frame = constant_frame()
 
     def next_frame(self):
         if self._stepper.step():
             self._frame = constant_frame()
-            for i in self._stars:
-                
+            for i in range(len(self._stars)):
+                star = self._stars[i]
+                print(star.x, star.y, star.z)
+                a = {(int(ceil(star.x)), int(ceil(star.y)), int(ceil(star.z))),
+                     (int(ceil(star.x)), int(ceil(star.y)), int(floor(star.z))),
+                     (int(ceil(star.x)), int(floor(star.y)), int(ceil(star.z))),
+                     (int(ceil(star.x)), int(floor(star.y)), int(floor(star.z))),
+                     (int(floor(star.x)), int(ceil(star.y)), int(ceil(star.z))),
+                     (int(floor(star.x)), int(ceil(star.y)), int(floor(star.z))),
+                     (int(floor(star.x)), int(floor(star.y)), int(ceil(star.z))),
+                     (int(floor(star.x)), int(floor(star.y)), int(floor(star.z)))}
+                j = np.array((star.x, star.y, star.z))
+                distances = [np.linalg.norm(j - np.array(_star)) for _star in a]
+                for _star, _dist in zip(a, distances):
+                    self._frame[_star] += MAX_INTENSITY * max(0, 1 - _dist)
+                star.z += star.speed
+                if star.z > 7 or star.z < 0:
+                    self._stars[i] = Star(random() * 7, random() * 7, 0, random() * 0.1 + 0.1)
 
         return self._frame
